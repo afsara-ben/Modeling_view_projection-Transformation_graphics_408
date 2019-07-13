8efc92be29881f16868483b4ef9fd2b3d429281b
afsara-ben <1505118.ab@ugrad.cse.buet.ac.bd>
2019-07-13 12:29:55 +0600

    stage 1,2,3 works

---
+++ offline2_v2.0/cmake-build-debug/CMakeFiles/offline2_v2_0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/offline2_v2_0.dir/main.cpp.o"
+  "offline2_v2_0.pdb"
+  "offline2_v2_0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/offline2_v2_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
